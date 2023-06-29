23eb0c80f1474d145804d0d1744cce08685e5b8d
Rutuja-vst <rutuja.kahandal@virtuososofttech.com>
2023-06-29 17:49:57 +0530

    updated data 29062023

---
+++ ExamPortalserver/src/main/java/com/vst/ExamPortalserverApplication.java
@@ -0,0 +1,13 @@
+package com.vst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalserverApplication.class, args);
+	}
+
+}
